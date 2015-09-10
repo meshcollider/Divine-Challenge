7e4f236cfd3e8ad9052ef50a228b662e251c9c05
harso121 <r123t.origin@gmail.com>
2015-09-11 01:51:49 +0200

    Update panel.php

---
+++ panel.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+		<meta charset="utf-8">
+      <title>Panel</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta name="description" content="Divine Challenge Panel">
+      <meta name="author" content="R123T">
+
+      <!-- CSS -->
+      <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+			<div id="header">
+				<div class="wrap">
+					<p>Welcome to the</p>
+					<h1>DIVINE CHALLENGE</h1>
+				</div>
+			</div>
+    </body>
+</html>
