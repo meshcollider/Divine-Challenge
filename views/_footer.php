0051d6b7214cfe559fa99c6f2a2608bd3ead284f
harso121 <r123t.origin@gmail.com>
2015-09-11 13:54:21 +0200

    Update _footer.php

--- views/_footer.php
+++ views/_footer.php
@@ -1,2 +1,5 @@
-</body>
-</html>
+<div id="footer">
+	<p class="alignLeft">Copyright (C) Divine 2015</p>
+	<p class="alignRight"> Support Thread (LINK)</p>
+	<div style="clear: both;"></div>
+</div>
