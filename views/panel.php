169cb7a8ba71905ce93985061e8eca48af29fd5c
harso121 <r123t.origin@gmail.com>
2015-09-11 13:56:40 +0200

    Finished Meshs Panel Design :)

--- views/panel.php
+++ views/panel.php
@@ -11,19 +11,19 @@
       <link rel="stylesheet" href="css/style1.css">
     </head>
     <body>
-		<?php include('_header.php'); ?>
-			<div id="content">
-				<p style="margin-bottom: 26px;">Challenge number: <?php echo $login->user_current_challenge; ?></p>
-				<div class="box">
-					<p id="boxTitle">Instructions:</p>
-					<p>INSTRUCTIONS	GO HERE. THIS WILL BE TAKEN FROM THE DATABASE</p>
-				</div>
-				<div id="answerForm">
-					<p>Solution:</p>
-					<input type="text" name="answer" class="textBox" value="">
-					<input type="button" class="btn" value="SUBMIT">
-				</div>
+	<?php include('_header.php'); ?>
+		<div id="content">
+			<p style="margin-bottom: 26px;">Challenge number: <?php echo $login->user_current_challenge; ?></p>
+			<div class="box">
+				<p id="boxTitle">Instructions:</p>
+				<p>INSTRUCTIONS	GO HERE. THIS WILL BE TAKEN FROM THE DATABASE</p>
 			</div>
-		<?php include('_footer.php'); ?>
+			<div id="answerForm">
+				<p>Solution:</p>
+				<input type="text" name="answer" class="textBox" value="">
+				<input type="button" class="btn" value="SUBMIT">
+			</div>
+		</div>
+	<?php include('_footer.php'); ?>
    </body>
 </html>
