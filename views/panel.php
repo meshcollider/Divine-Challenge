82d07c9fbb9526ac444e69bb2a4137702ad92911
harso121 <r123t.origin@gmail.com>
2015-09-11 13:57:51 +0200

    Finished Meshs Panel Design :)

--- views/panel.php
+++ views/panel.php
@@ -2,7 +2,7 @@
 <html lang="en">
     <head>
       <meta charset="utf-8">
-      <title>Challlenge Panel - Divine Challenge</title>
+      <title>Challenge Panel - Divine Challenge</title>
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta name="description" content="Divine Challenge Panel">
       <meta name="author" content="Divine">
