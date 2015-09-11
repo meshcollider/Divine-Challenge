dd1e9c5dd65ae7e6f5a4fac22f081cd463a27870
MeshCollider <dobsonsa68@gmail.com>
2015-09-11 12:04:40 +1200

    Restructuring

--- panel.php
+++ panel.php
@@ -2,13 +2,13 @@
 <html lang="en">
     <head>
       <meta charset="utf-8">
-      <title>Panel</title>
+      <title>Panel - Divine Challenge</title>
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta name="description" content="Divine Challenge Panel">
-      <meta name="author" content="R123T">
+      <meta name="author" content="Divine">
 
       <!-- CSS -->
-      <link rel="stylesheet" href="style1.css">
+      <link rel="stylesheet" href="css/style1.css">
     </head>
     <body>
 	<div id="header">
