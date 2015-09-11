feea990075526747b26b6c72b5bca65911bef7ef
harso121 <r123t.origin@gmail.com>
2015-09-11 12:27:29 +0200

    Update _header.php

--- views/_header.php
+++ views/_header.php
@@ -3,32 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <title>php-login-advanced</title>
-    <style type="text/css">
-        /* just for the demo */
-        body {
-            font-family: Arial, sans-serif;
-            font-size: 12px;
-            margin: 10px;
-        }
-        label {
-            position: relative;
-            vertical-align: middle;
-            bottom: 1px;
-        }
-        input[type=text],
-        input[type=password],
-        input[type=submit],
-        input[type=email] {
-            display: block;
-            margin-bottom: 15px;
-        }
-        input[type=checkbox] {
-            margin-bottom: 15px;
-        }
-    </style>
 </head>
 <body>
-
+    <div id="header">
+		<p>Welcome to the</p>
+		<h1>DIVINE CHALLENGE</h1>
+	</div>
 <?php
 // show potential errors / feedback (from login object)
 if (isset($login)) {
@@ -60,3 +40,4 @@ if (isset($registration)) {
     }
 }
 ?>
+</body>
