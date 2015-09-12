f6e9329a2ee6adfee33d21a318662490f64f564c
MeshCollider <dobsonsa68@gmail.com>
2015-09-12 16:19:30 +1200

    Removing email address from registration

--- classes/Registration.php
+++ classes/Registration.php
@@ -40,7 +40,7 @@ class Registration
 
         // if we have such a POST request, call the registerNewUser() method
         if (isset($_POST["register"])) {
-            $this->registerNewUser($_POST['user_name'], $_POST['user_email'], $_POST['user_password_new'], $_POST['user_password_repeat'], $_POST['user_uid'], $_POST["captcha"]);
+            $this->registerNewUser($_POST['user_name'], $_POST['user_password_new'], $_POST['user_password_repeat'], $_POST['user_uid'], $_POST["captcha"]);
         // if we have such a GET request, call the verifyNewUser() method
         } else if (isset($_GET["id"]) && isset($_GET["verification_code"])) {
             $this->verifyNewUser($_GET["id"], $_GET["verification_code"]);
@@ -78,11 +78,10 @@ class Registration
      * handles the entire registration process. checks all error possibilities, and creates a new user in the database if
      * everything is fine
      */
-    private function registerNewUser($user_name, $user_email, $user_password, $user_password_repeat, $user_uid, $captcha)
+    private function registerNewUser($user_name, $user_password, $user_password_repeat, $user_uid, $captcha)
     {
         // we just remove extra space on username and email
         $user_name  = trim($user_name);
-        $user_email = trim($user_email);
 
         // check provided data validity
         // TODO: check for "return true" case early, so put this first
@@ -102,28 +101,20 @@ class Registration
             $this->errors[] = MESSAGE_USERNAME_BAD_LENGTH;
         } elseif (!preg_match('/^[a-z\d]{2,64}$/i', $user_name)) {
             $this->errors[] = MESSAGE_USERNAME_INVALID;
-        } elseif (empty($user_email)) {
-            $this->errors[] = MESSAGE_EMAIL_EMPTY;
-        } elseif (strlen($user_email) > 64) {
-            $this->errors[] = MESSAGE_EMAIL_TOO_LONG;
-        } elseif (!filter_var($user_email, FILTER_VALIDATE_EMAIL)) {
-            $this->errors[] = MESSAGE_EMAIL_INVALID;
 
         // finally if all the above checks are ok
         } else if ($this->databaseConnection()) {
             // check if username or email already exists
-            $query_check_user_name = $this->db_connection->prepare('SELECT user_name, user_email FROM users WHERE user_name=:user_name OR user_email=:user_email OR user_hf_uid=:uid');
+            $query_check_user_name = $this->db_connection->prepare('SELECT user_name FROM users WHERE user_name=:user_name');
             $query_check_user_name->bindValue(':user_name', $user_name, PDO::PARAM_STR);
-            $query_check_user_name->bindValue(':user_email', $user_email, PDO::PARAM_STR);
-            $query_check_user_name->bindValue(':uid', $user_uid, PDO::PARAM_STR);
             $query_check_user_name->execute();
             $result = $query_check_user_name->fetchAll();
 
-            // if username or/and email find in the database
+            // if username is already in the database
             // TODO: this is really awful!
             if (count($result) > 0) {
                 for ($i = 0; $i < count($result); $i++) {
-                    $this->errors[] = ($result[$i]['user_name'] == $user_name) ? MESSAGE_USERNAME_EXISTS : MESSAGE_EMAIL_ALREADY_EXISTS;
+                    $this->errors[] = MESSAGE_USERNAME_EXISTS;
                 }
             } else {
                 // check if we have a constant HASH_COST_FACTOR defined (in config/hashing.php),
@@ -139,10 +130,9 @@ class Registration
                 $user_activation_hash = sha1(uniqid(mt_rand(), true));
 
                 // write new users data into database
-                $query_new_user_insert = $this->db_connection->prepare('INSERT INTO users (user_name, user_password_hash, user_email, user_registration_ip, user_registration_datetime, user_hf_uid) VALUES(:user_name, :user_password_hash, :user_email, :user_registration_ip, now(), :uid)');
+                $query_new_user_insert = $this->db_connection->prepare('INSERT INTO users (user_name, user_password_hash, user_registration_ip, user_registration_datetime, user_hf_uid) VALUES(:user_name, :user_password_hash, :user_registration_ip, now(), :uid)');
                 $query_new_user_insert->bindValue(':user_name', $user_name, PDO::PARAM_STR);
                 $query_new_user_insert->bindValue(':user_password_hash', $user_password_hash, PDO::PARAM_STR);
-                $query_new_user_insert->bindValue(':user_email', $user_email, PDO::PARAM_STR);
                 $query_new_user_insert->bindValue(':user_registration_ip', $_SERVER['REMOTE_ADDR'], PDO::PARAM_STR);
                 $query_new_user_insert->bindValue(':uid', $user_uid, PDO::PARAM_STR);
                 $query_new_user_insert->execute();
--- views/register.php
+++ views/register.php
@@ -20,13 +20,11 @@
 		<?php if (!$registration->registration_successful && !$registration->verification_successful) { ?>
 		<form method="post" action="register.php" name="registerform">
 			<label for="user_uid">Hackforums UID</label><br />
-			<input id="user_uid" type="number" step="1" min="1" name="user_uid" required /><br /><br />
+			<input id="user_uid" type="number" step="1" min="1" name="user_uid" required /><br />
+			Make sure this is correct, you will need this to claim prizes and in case you forget your password<br /><br />
 
 			<label for="user_name"><?php echo WORDING_REGISTRATION_USERNAME; ?></label><br />
 			<input id="user_name" type="text" pattern="[a-zA-Z0-9]{2,64}" name="user_name" required /><br /><br />
-
-			<label for="user_email"><?php echo WORDING_REGISTRATION_EMAIL; ?></label><br />
-			<input id="user_email" type="email" name="user_email" required /><br /><br />
 
 			<label for="user_password_new"><?php echo WORDING_REGISTRATION_PASSWORD; ?></label><br />
 			<input id="user_password_new" type="password" name="user_password_new" pattern=".{6,}" required autocomplete="off" /><br /><br />
