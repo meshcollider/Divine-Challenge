400351a9e8a84204d69e4e86f685b411e19eb962
MeshCollider <dobsonsa68@gmail.com>
2015-09-12 12:08:03 +1200

    Update 01-create-challenge-table.sql

--- _installation/01-create-challenge-table.sql
+++ _installation/01-create-challenge-table.sql
@@ -3,7 +3,8 @@ CREATE TABLE IF NOT EXISTS `challenges` (
  `challenge_author` varchar(64) COLLATE utf8_unicode_ci NOT NULL COMMENT 'authors name',
  `challenge_instructions` TEXT NOT NULL COMMENT 'instructions for the challenge',
  `challenge_answer` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'answer to the challenge',
- `challenge_status` tinyint(1) NOT NULL DEFAULT '1' COMMENT 'is the challenge actice',
- PRIMARY KEY (`user_id`),
+ `challenge_status` tinyint(1) NOT NULL DEFAULT '1' COMMENT 'is the challenge active',
+ `challenge_dynamic` boolean DEFAULT 'false' COMMENT 'is the answer dynamic',
+ PRIMARY KEY (`challenge_id`),
  UNIQUE KEY `challenge_id` (`challenge_id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
