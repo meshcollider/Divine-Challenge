55e789819cb3a0c655ca655e58e6db3c86b79742
MeshCollider <dobsonsa68@gmail.com>
2015-09-28 16:39:20 +1300

    Adding the new design from Shadow which needs to be implemented

---
+++ js/lol.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $('#particles').particleground({
+    dotColor: '#9C8DF7',
+    lineColor: '#9C8DF7'
+  });
+  $('.intro').css({
+    'margin-top': -($('.intro').height() / 2)
+  });
+});
