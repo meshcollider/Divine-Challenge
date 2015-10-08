a13a4534a38e1bba3ba3bc08adf8d5e8f9fd7c11
MeshCollider <dobsonsa68@gmail.com>
2015-10-09 11:11:48 +1300

    Setting up the countdown

---
+++ wait.php
@@ -0,0 +1,25 @@
+<!doctype html> 
+<html lang="en" class="no-js"> 
+<head> 
+  <meta charset="UTF-8" /> 
+  <title>Divine</title> 
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/reset.css" />
+  <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+  <script type='text/javascript' src='js/jquery-1.11.1.min.js'></script>
+  <script type='text/javascript' src='js/jquery.particleground.min.js'></script>
+  <script type='text/javascript' src='js/lol.js'></script>
+</head> 
+
+<body>
+
+<div id="particles">
+  <div class="intro">
+    <h1>Divine Challenge</h1>
+	<p>The challenge starts in:</p><br />
+    <img src="countdown.php" />
+  </div>
+</div>
+
+</body> 
+</html> 
