b30d7330840b3afe12fb56ce9871049322b085da
harso121 <r123t.origin@gmail.com>
2015-10-10 13:22:41 +0200

    Change gray graph text to white

--- stats.php
+++ stats.php
@@ -84,7 +84,11 @@
 		<!-- CSS -->
 		<link rel="stylesheet" href="css/style3.css">
 		<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css">
-			
+		<style>
+			.morris-default-style {
+				color: #FFF;
+			}
+		</style>	
 		<!-- JS -->
 		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
 		<script src="http://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
